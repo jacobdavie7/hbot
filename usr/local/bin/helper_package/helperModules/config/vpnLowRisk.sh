0ee502781fdc8ce233356b7557336343d72dd6ae
Jacob Davie <github@accountalias.com>
2022-12-12 02:30:50 -0600

    added vpn modual - low risk

---
+++ usr/local/bin/helper_package/helperModules/config/vpnLowRisk.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+function vpnLowRisk ()
+{
+    # Keys
+        echo -e "\nSet Wireguard Key Regen to 24 hours"
+            mullvad tunnel wireguard key rotation-interval set 24
+
+        echo -e "\nRegen Wireguard Key"
+            mullvad tunnel wireguard key regenerate
+
+    # Location
+        echo -e "\nSet US Relay"
+            mullvad relay set location us
+    
+    # VPN Settings
+        echo -e "\nAllow local LAN Devices"
+            mullvad lan set allow
+    
+        echo -e "\nSet DNS to Block: Ads, Maleware, Tracking, Gambling, Adult"
+            mullvad dns set default --block-ads --block-malware --block-trackers --block-gambling --block-adult-content
+
+        echo -e "\nSet Protocol to Wireguard"
+            mullvad relay set tunnel-protocol wireguard
+       
+    # Wireguard Settings
+        echo -e "\nSet Port"
+            mullvad relay set tunnel wireguard --port 51820
+
+        echo -e "\nObfuscation Auto"
+            mullvad obfuscation set mode auto
+
+        echo -e "\nDisable Quantum Resistance"
+            mullvad tunnel wireguard quantum-resistant-tunnel set off
+
+        echo -e "\nDisable MultiHop"
+            mullvad relay set tunnel wireguard --entry-location none
+
+    # Connect
+        echo -e "\nEnable Auto Connect"
+            mullvad auto-connect set on
+
+        echo -e "\nConnecting to Mullvad"
+            mullvad connect
+}
