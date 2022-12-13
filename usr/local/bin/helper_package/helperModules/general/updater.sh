5a55f9999ca86002bbcb769a04dc0f171ab5d428
Jacob Davie <github@accountalias.com>
2022-12-12 20:49:35 -0600

    removed old code

--- usr/local/bin/helper_package/helperModules/general/updater.sh
+++ usr/local/bin/helper_package/helperModules/general/updater.sh
@@ -17,24 +17,4 @@ function updater
     
     echo -e "\n\e[95;4msnap\e[39;24m"
         snap refresh
-
-#    echo -e "\n\e[44mExecuting 'Updater' Function\e[49m"
-#        #apt
-#        echo -e "\n\e[95;4mapt\e[39;24m"
-#            sudo apt update
-#            UPGRADES=$(apt list --upgradeable | wc -l)
-#            if [ "$UPGRADES" != "1" ]; then
-#                echo -e "\n\e[4mApt Packages that can be Upgraded:\e[39;24m"
-#                    apt list --upgradeable
-#                echo -e "\n\e[4mRun Apt Upgradeable and Autoremove? (y or n)\e[39;24m"
-#                    read ANS
-#                    if [ "$ANS" == "y" ]; then
-#                        sudo apt upgrade -y
-#                        sudo apt autoremove -y
-#                    fi
-#            fi
-#
-#        #snap
-#        echo -e "\n\e[95;4msnap\e[39;24m"
-#            sudo snap refresh
 }
