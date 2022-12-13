c8c83b671326e3fc694092290172dab084c5046c
Jacob Davie <github@accountalias.com>
2022-12-13 06:51:33 -0600

    added modual

---
+++ usr/local/bin/helper_package/helperModules/config/vpnHighRisk.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+function vpnHighRisk ()
+{
+
+    # Deploy Limited Firewall Ruleset
+        echo -e "\nCall the Home Limited Firewall Ruleset"
+            firewallHomeLimited
+
+    # Keys
+        echo -e "\nSet Wireguard Key Regen to 8 hours"
+            mullvad tunnel wireguard key rotation-interval set 8
+
+        echo -e "\nRegen Wireguard Key"
+            mullvad tunnel wireguard key regenerate
+
+    # Location
+        echo -e "\nSet US Relay"
+            mullvad relay set location us
+    
+    # VPN Settings
+        echo -e "\nBlock local LAN Devices"
+            mullvad lan set block
+    
+        echo -e "\nSet DNS to Block: Ads, Maleware, Tracking, Gambling, Adult"
+            mullvad dns set default --block-ads --block-malware --block-trackers --block-gambling --block-adult-content
+
+        echo -e "\nSet Protocol to Wireguard"
+            mullvad relay set tunnel-protocol wireguard
+       
+    # Wireguard Settings
+        echo -e "\nSet Port"
+            mullvad relay set tunnel wireguard --port 53
+
+        echo -e "\nObfuscation On UDP 2 TCP"
+            mullvad obfuscation set mode udp2tcp
+
+    #Quantum Resistance is highly experimental. Currently it is too buggy to be reliable. Note I WILL need to specify EXACT server that supports Quantum
+        #echo -e "\nEnable Quantum Resistance"
+            #mullvad tunnel wireguard quantum-resistant-tunnel set on
+
+        echo -e "\nEnable MultiHop (Entry in Sweden)"
+            mullvad relay set tunnel wireguard --entry-location se
+
+    # Connect
+        echo -e "\nEnable Auto Connect"
+            mullvad auto-connect set on
+
+        echo -e "\nConnecting to Mullvad"
+            mullvad connect
+}
