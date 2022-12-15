9ed9d1c794ada37affc3950fe1835c25549ddd6d
Jacob Davie <github@accountalias.com>
2022-12-14 23:00:35 -0600

    removed ablity to run without root

--- usr/local/bin/helper_package/helperModules/firewalls/firewallPersistentSave.sh
+++ usr/local/bin/helper_package/helperModules/firewalls/firewallPersistentSave.sh
@@ -8,13 +8,7 @@ function firewallPersistentSave
         echo -e "\e[31mRun \e[4mapt install iptables-persistent\e[0m\e[31m to keep current ruleset after restart.\e[39m"
         echo -e "\e[31mUpon install, only the current ruleset will be saved. Changes to rules outside of the script will not be saved.\e[39m"
     else
-        if [ "$EVEVATE" == "root" ]; then
-            iptables-save > /etc/iptables/rules.v4
-        else
-            sudo chmod 666 /etc/iptables/rules.v4
-            sudo -i iptables-save > /etc/iptables/rules.v4
-            sudo chmod 644 /etc/iptables/rules.v4
-        fi
+        iptables-save > /etc/iptables/rules.v4
         echo -e "\n\e[32miptables-persistent found! Ruleset saved and will be persistent on restart.\e[39m\n"
     fi
 }
