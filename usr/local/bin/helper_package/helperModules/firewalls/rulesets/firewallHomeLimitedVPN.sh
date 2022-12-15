1e974f419b79b3ec0501999f56d3a6293253e039
Jacob Davie <github@accountalias.com>
2022-12-15 01:30:53 -0600

    created file

---
+++ usr/local/bin/helper_package/helperModules/firewalls/rulesets/firewallHomeLimitedVPN.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+function firewallHomeLimitedVPN
+{
+    echo -e "\n\e[44mDeploying Limited Home Firewall Rules\e[49m"
+
+    echo -e "\nSetting default policy to DROP"
+        iptables -P OUTPUT DROP
+        iptables -P INPUT DROP
+        iptables -P FORWARD DROP
+
+    echo -e "\nFlushing all chains"
+        iptables -F
+
+    echo -e "\nDROP bad packets"
+            echo " - XMAS       (IN)"
+                iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP -m comment --comment "DROP outgoing XMAS"
+            echo " - NULL       (IN)"
+                iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP -m comment --comment "DROP outgoing NULL"
+            echo " - INVALID    (IN)"
+                iptables -A INPUT -m conntrack --ctstate INVALID -j DROP -m comment --comment "DROP anything marked INVALID"
+            echo " - Fragmented (IN)"
+                iptables -A INPUT -f -j DROP -m comment --comment "DROP Fragmented"
+            echo " - NEW != SYN (IN)"
+                iptables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP -m comment --comment "DROP any NEW connections that do NOT start with SYN"
+            echo " - SYN Flood  (IN)"
+                iptables -A INPUT -p tcp --syn -m hashlimit --hashlimit-name synFlood --hashlimit-above 30/s -j DROP -m comment --comment "LIMIT SYN to 30/sec"
+
+    echo -e "\nALLOW anything marked RELATED/ESTABLISHED"
+        iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "ACCEPT incoming RELATED/ESTABLISHED"
+        iptables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "ACCEPT outgoing RELATED/ESTABLISHED"
+
+    echo "ALLOW everything on loopback"
+        iptables -A INPUT -s 127.0.0.1 -j ACCEPT -m comment --comment "ACCEPT all incoming on loopback"
+        iptables -A OUTPUT -d 127.0.0.1 -j ACCEPT -m comment --comment "ACCEPT all outgoing on loopback"
+
+    echo -e "\nALLOW services OUT"
+        echo " - HTTP              (OUT)"
+            iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http"
+        echo " - HTTPS             (OUT)"
+            iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https"
+        echo " - DNS               (OUT)"
+            iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns"
+       echo " - Wireguard          (OUT)"   # still needed when using port 53 with multihop
+            iptables -A OUTPUT -p udp --dport 51820 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new wireguard"
+        echo " - PING              (OUT)"   # needed to locate server
+            iptables -A OUTPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing ping request"
+    
+    firewallPersistentSave
+}
+
+
+
