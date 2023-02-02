d66dbc3acfdb04377d4993025966052f4cf37a24
Jacob Davie <github@accountalias.com>
2023-02-02 01:13:33 -0600

    Changed from port 53 to 51820

--- usr/local/bin/helper_package/helperModules/config/vpn/vpnMobile.sh
+++ usr/local/bin/helper_package/helperModules/config/vpn/vpnMobile.sh
@@ -25,8 +25,8 @@ function vpnMobile ()
             mullvad relay set tunnel-protocol wireguard
        
     # Wireguard Settings
-        echo -e "\nSet Port"
-            mullvad relay set tunnel wireguard --port 53
+        echo -e "\nSet Port to 51820"
+            mullvad relay set tunnel wireguard --port 51820
 
         echo -e "\nObfuscation Auto"
             mullvad obfuscation set mode auto
