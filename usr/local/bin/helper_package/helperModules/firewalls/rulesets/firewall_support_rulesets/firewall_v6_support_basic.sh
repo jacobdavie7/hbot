f9dfedcd25af1dc40a5258dd3a068529b7a0dd40
Jacob Davie <github@accountalias.com>
2023-09-22 01:13:18 -0500

    removed allow related/established

--- usr/local/bin/helper_package/helperModules/firewalls/rulesets/firewall_support_rulesets/firewall_v6_support_basic.sh
+++ usr/local/bin/helper_package/helperModules/firewalls/rulesets/firewall_support_rulesets/firewall_v6_support_basic.sh
@@ -12,25 +12,40 @@ function firewall_v6_support_basic
         echo -e "\nFlushing all IPv6 chains"
             ip6tables -F
 
-        echo -e "\nDROP bad packets"
-                echo " - XMAS       (DROP - IN)"
-                    ip6tables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP -m comment --comment "DROP outgoing XMAS"
-                echo " - NULL       (DROP - IN)"
-                    ip6tables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP -m comment --comment "DROP outgoing NULL"
-                echo " - INVALID    (DROP - IN)"
-                    ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP -m comment --comment "DROP anything marked INVALID"
-                echo " - Fragmented (DROP - IN)"
-                    ip6tables -A INPUT -m frag -j DROP -m comment --comment "DROP Fragmented"
-                echo " - NEW != SYN (DROP - IN)"
-                    ip6tables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP -m comment --comment "DROP any NEW connections that do NOT start with SYN"
-                echo " - SYN Flood  (DROP - IN)"
-                    ip6tables -A INPUT -p tcp --syn -m hashlimit --hashlimit-name synFlood --hashlimit-above 30/s -j DROP -m comment --comment "LIMIT SYN to 30/sec"
+#        echo -e "\nDROP bad packets"
+#                echo " - XMAS       (DROP - IN)"
+#                    ip6tables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP -m comment --comment "DROP outgoing XMAS"
+#                echo " - NULL       (DROP - IN)"
+#                    ip6tables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP -m comment --comment "DROP outgoing NULL"
+#                echo " - INVALID    (DROP - IN)"
+#                    ip6tables -A INPUT -m conntrack --ctstate INVALID -j DROP -m comment --comment "DROP anything marked INVALID"
+#                echo " - Fragmented (DROP - IN)"
+#                    ip6tables -A INPUT -m frag -j DROP -m comment --comment "DROP Fragmented"
+#                echo " - NEW != SYN (DROP - IN)"
+#                    ip6tables -A INPUT -p tcp ! --syn -m conntrack --ctstate NEW -j DROP -m comment --comment "DROP any NEW connections that do NOT start with SYN"
+#                echo " - SYN Flood  (DROP - IN)"
+#                    ip6tables -A INPUT -p tcp --syn -m hashlimit --hashlimit-name synFlood --hashlimit-above 30/s -j DROP -m comment --comment "LIMIT SYN to 30/sec"
 
-        echo -e "\nACCEPT everything marked RELATED/ESTABLISHED"
-            ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "ACCEPT incoming RELATED/ESTABLISHED"
-            ip6tables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "ACCEPT outgoing RELATED/ESTABLISHED"
-    
-        echo "ACCEPT everything on loopback"
-            ip6tables -A INPUT -i lo -j ACCEPT -m comment --comment "ACCEPT all incoming on loopback"
-            ip6tables -A OUTPUT -o lo -j ACCEPT -m comment --comment "ACCEPT all outgoing on loopback"
+#            ip6tables -A INPUT -s fc00::/7 -j DROP -m comment --comment "DROP from unique local address"
+#            ip6tables -A OUTPUT -d fc00::/7 -j DROP -m comment --comment "DROP to unique local address"
+#
+#            ip6tables -A INPUT -s fd00::/8 -j DROP -m comment --comment "DROP from unique local address"
+#            ip6tables -A OUTPUT -d fd00::/8 -j DROP -m comment --comment "DROP to unique local address"
+#
+#            ip6tables -A INPUT -s ::/128 -j DROP -m comment --comment "DROP from unspecified"
+#            ip6tables -A OUTPUT -d ::/128 -j DROP -m comment --comment "DROP to unspecified"
+#
+#            ip6tables -A INPUT -s ::ffff:0:0/96 -j DROP -m comment --comment "DROP from ipv4 to ipv6 map"
+#            ip6tables -A OUTPUT -d ::ffff:0:0/96 -j DROP -m comment --comment "DROP to ipv4 to ipv6 map"
+#            
+#            ip6tables -A INPUT -s fe80::/10 -j DROP -m comment --comment "DROP from link-local"
+#            ip6tables -A OUTPUT -d fe80::/10 -j DROP -m comment --comment "DROP to link-local"
+#        
+#        echo -e "\nACCEPT everything marked RELATED/ESTABLISHED"
+#            ip6tables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "ACCEPT incoming RELATED/ESTABLISHED"
+#            ip6tables -A OUTPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT -m comment --comment "ACCEPT outgoing RELATED/ESTABLISHED"
+#    
+#        echo "ACCEPT everything on loopback"
+#            ip6tables -A INPUT -i lo -j ACCEPT -m comment --comment "ACCEPT all incoming on loopback"
+#            ip6tables -A OUTPUT -o lo -j ACCEPT -m comment --comment "ACCEPT all outgoing on loopback"
 }
