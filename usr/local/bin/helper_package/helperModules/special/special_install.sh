a5e5c37ca38809f1007108ccc654b811664886d4
Jacob Davie <github@accountalias.com>
2023-10-18 05:15:16 -0500

    updated packages

--- usr/local/bin/helper_package/helperModules/special/special_install.sh
+++ usr/local/bin/helper_package/helperModules/special/special_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function special_install
+function install
 {
     echo -e "\nRun full install and setup script? (yes or n)"
     read ANS
@@ -9,10 +9,11 @@ function special_install
     fi
 
     # updates
-        general_updater
+        updater
 
 # add contrib and non-free repos, change http to https, do this before steam and nvidia
 # install nvidia-detect, grab what it wants you to install, do this uphere, before steam
+
 
     # install packages
 
@@ -21,37 +22,23 @@ function special_install
                 UTILITIES=(
                     curl                            # interact with urls
                     dnsutils                        # contains dig
-                    etherwake                       # send magic packets
-                    ethtool                         # view nic info
                     ffmpeg                          # video converter/media formats
-                    fonts-unfonts-core              # display more lanuages
                     git                             # content tracker
                     gzip                            # gzip compression
                     hdparm                          # get drive parameters
                     htop                            # proccess viewer
-                    ibus-hangul                     # input korean
                     iptables                        # firewall
                     iptables-persistent             # make ruleset persistent upon restart
-                    #libc++1                         # keep discord from crashing
-                    #libc++1-14                      # keep discord from crashing
-                    #libc++abi1-14                   # keep discord from crashing
-                    #libpcslite-dev                  # smartcard access via pc/sc (proxmark)
-                    #libreadline-dev                 # consistent ui to recall lines of previously input (proxmark)
-                    #libunwind-14                    # keep discord from crashing
                     lshw                            # list hardware, view cpu details
-                    #menulibre                       # edit applications whisker menu can open (add app images)
                     ncdu                            # file sizes
-                    #network-manager-gnome           # panel applet
-                    ntp                             # get time from ntp server
+                    network-manager-gnome           # panel applet
                     ranger                          # terminal file explorer
-                    #software-properties-common      # repo manager
+                    software-properties-common      # repo manager
                     sudo                            # super
                     tree                            # show directory structure
                     unzip                           # unzip files
-                    #v4l2loopback-dkms               # video loopback device - needed for obs
                     vim                             # text editor
                     whois                           # make whois lookups
-                    #zenity                          # draw windows for ibus
                     zip                             # create zip files
                 )
                 for U in "${UTILITIES[@]}"
@@ -60,30 +47,25 @@ function special_install
                 done
 
                 APPLICATIONS=(
-                    firefox-esr                     # web browser
-                    #flameshot                       # screenshots
-                    #galculator                      # simple calculator
-                    #gnome-disk-utility              # gui disk manager
-                    #gparted                         # gui partition manager
-                    #gqrx-sdr                        # sdr
-                    #gummi                           # laTeX editor
+                    gnome-disk-utility              # gui disk manager
+                    gparted                         # gui partition manager
                     keepassxc                       # password manager
-                    libreoffice                     # productivity suite
-                    libreoffice-gtk3                # make libreoffice look better
-                    #obs-studio                      # screencast
-                    #pulseeffects                    # effects
                     qdirstat                        # visualize storage
                     screen                          # screen manager with terminal emulation
-                    #speedcrunch                     # advanced calculator
-                    steam                           # games     #non-free repo
-                    #thunderbird                     # email client
                     vlc                             # media player
-                    #wireshark                       # analyze packets and network traffic
-                    xournalpp                       # pdf editing
                 )
                 for A in "${APPLICATIONS[@]}"
                 do
                     apt install -y $A
+                done
+
+                PACKAGE_MANAGERS=(
+                    flatpak
+                #    snapd
+                )
+                for P in "${PACKAGE_MANAGERS[@]}"
+                do
+                    apt install -y $P
                 done
 
                 FUN=(
@@ -100,28 +82,66 @@ function special_install
                     apt install -y $F
                 done
 
+                EXTRA=(
+                    fonts-unfonts-core              # display more lanuages
+                    ibus-hangul                     # input korean
+                    libpcslite-dev                  # smartcard access via pc/sc (proxmark)
+                    libreadline-dev                 # consistent ui to recall lines of previously input (proxmark)
+                    menulibre                       # edit applications whisker menu can open (add app images)
+                    ntp                             # get time from ntp server
+                    v4l2loopback-dkms               # video loopback device - needed for obs
+                    zenity                          # draw windows for ibus
+                    flameshot                       # screenshots
+                    firefox-esr                     # web browser
+                    galculator                      # simple calculator
+                    gqrx-sdr                        # sdr
+                    libreoffice                     # productivity suite
+                    libreoffice-gtk3                # make libreoffice look better
+                    obs-studio                      # screencast
+                    pulseeffects                    # effects
+                    steam                           # games     #non-free repo
+                    thunderbird                     # email client
+                    wireshark                       # analyze packets and network traffic
+                    gummi                           # laTeX editor
+                    speedcrunch                     # advanced calculator
+                )
+                for E in "${EXTRA[@]}"
+                do
+                    apt install -y $E
+                done
+
+
+        # flatpak
+            echo -e "\n Install FLATPAK Packages\n"
+
+                # add flathub repo
+                    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+                FLATPAK=(
+                    com.github.xournalpp.xournalpp  # xournalpp, pdf editor
+                    com.github.tchx84.Flatseal      # flatseal, gui for flapak permissons
+                    com.spotify.Client              # spotify, music
+                    com.valvesoftware.Steam         # steam, games
+                    com.visualstudio.code-oss       # IDE
+                    com.github.wwmm.easyeffects     # sound proccesing for pipewire
+                    )
+                for F in "${FLATPAK[@]}"
+                do
+                    flatpak install flathub $F
+                done
+
         # local
             echo -e "\n Install LOCAL APT Packages\n"
-
-            # discord
-                wget -O /tmp/discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" # -O used to put into file, page does not give .deb file
-                apt install /tmp/./discord.deb
             
-            # visual studio
-                wget -O /tmp/vscode.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64" # -O used to put into file, page does not give .deb file
-                apt install /tmp/./vscode.deb
-                
-            # spotify
-                curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
-                echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
-                sudo apt-get update && sudo apt install spotify-client
-
             # mullvad
-                wget -O /tmp/mullvadVPN.deb "https://mullvad.net/en/download/app/deb/latest" # -O used to put into file, page does not give .deb file
-                apt install /tmp/./mullvadVPN.deb
-            
+                mkdir /tmp/packages
+                chmod 170 /tmp/packages
+                chown _apt:jacob /tmp/packages
+                wget -O /tmp/packages/mullvad.deb https://mullvad.net/en/download/app/deb/latest -P /tmp/packages # -O used to put into file, page does not give .deb file
+                apt install /tmp/packages/./mullvad.deb
+                
             # virtual box
-                # do this manually, no link to latest, just specific verisons
+            
 
     # setup unattended upgrades
         sudo dpkg-reconfigure --priority=low unattended-upgrades
@@ -145,13 +165,10 @@ function special_install
     # ntp
         systemctl start ntp
         systemctl enable ntp
-        timedatectl set-ntp true
 
     # setup firewall
         firewallHome
 
-    # static route
-        nmcli connection modify "Wired Connection 1" ipv4.routes "10.0.3.0/24 10.0.4.1"
     # set hostname
         
     # from drawing module, put this here so don't need sudo.    
@@ -166,8 +183,8 @@ function special_install
             else
                 echo -e "\nEntry in config found!"
             fi
-}
 
+}
 
    #Clone GitHub Repo
         #git clone https://github.com/AdnanHodzic/displaylink-debian.git /tmp/displaylink-debian
@@ -177,4 +194,3 @@ function special_install
     # Should look like something below:
     # Provider 1: id: 0x138 cap: 0x2, Sink Output crtcs: 1 outputs: 1 associated providers: 0 name:modesetting
     # ...Goes to provider 4, looks like provider 1                                          ^ This 0 should change to 1 after the commands below
-
