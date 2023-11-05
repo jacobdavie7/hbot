c0bb7e1ec2ff52c6a7780bf83fef7ec6e28d439e
Jacob Davie <github@accountalias.com>
2023-11-04 19:04:19 -0500

    updated how to gather user acount

--- usr/local/bin/helper_package/helperModules/support/support_userAccount.sh
+++ usr/local/bin/helper_package/helperModules/support/support_userAccount.sh
@@ -2,5 +2,6 @@
 
 function support_userAccount
 {
-    USER_ACCOUNT=$(cat /etc/passwd | grep "1000" | cut -d':' -f1)
+    USER_ACCOUNT=$(id -n -u)
+    # USER_ACCOUNT=$(cat /etc/passwd | grep "1000" | cut -d':' -f1)
 }
