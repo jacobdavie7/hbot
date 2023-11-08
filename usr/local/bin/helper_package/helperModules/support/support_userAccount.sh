462e16ab61c65640c57cad7c2309bb486eb24694
Jacob Davie <github@accountalias.com>
2023-11-08 09:18:09 -0600

    switched function to get user account

--- usr/local/bin/helper_package/helperModules/support/support_userAccount.sh
+++ usr/local/bin/helper_package/helperModules/support/support_userAccount.sh
@@ -2,6 +2,6 @@
 
 function support_userAccount
 {
-    USER_ACCOUNT=$(id -n -u)
-    # USER_ACCOUNT=$(cat /etc/passwd | grep "1000" | cut -d':' -f1)
+    USER_ACCOUNT=$(cat /etc/passwd | grep "1000" | cut -d':' -f1)
+    # USER_ACCOUNT=$(id -n -u)  # if run as root, will return root, not user account
 }
