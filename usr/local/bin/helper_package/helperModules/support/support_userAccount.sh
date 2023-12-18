23653db48735e8ed521550d4520a54b92da064da
Jacob Davie <github@accountalias.com>
2023-12-18 17:36:01 -0600

    added comment

--- usr/local/bin/helper_package/helperModules/support/support_userAccount.sh
+++ usr/local/bin/helper_package/helperModules/support/support_userAccount.sh
@@ -2,6 +2,7 @@
 
 function support_userAccount
 {
+    # Can't user $USER when run as root, this is still needed
     USER_ACCOUNT=$(cat /etc/passwd | grep "1000" | cut -d':' -f1)
     # USER_ACCOUNT=$(id -n -u)  # if run as root, will return root, not user account
 }
