9ad7079e1e53984a0e6dc807237f60f4ae998005
Jacob Davie <github@accountalias.com>
2024-01-29 14:53:38 -0600

    formatting

--- usr/local/bin/hbot_package/hbotModules/firewalls/home_rulesets/firewall_home_lax.sh
+++ usr/local/bin/hbot_package/hbotModules/firewalls/home_rulesets/firewall_home_lax.sh
@@ -5,7 +5,7 @@ function firewall_home_lax
     firewall_header
     
     echo -e "\n\e[44mDeploying Lax Home Firewall Rules\e[49m"
-        
+
         echo -e "\nSetting default OUTPUT policy to ACCEPT"    
             iptables -P OUTPUT ACCEPT
 
