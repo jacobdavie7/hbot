c77369d3fe500f75483a9937429a9a69de20d7aa
Jacob Davie <github@accountalias.com>
2024-02-01 01:51:16 -0600

    updated ruleset

--- usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_backup.sh
+++ usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_backup.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
-function firewall_server_backup
+function firewall_server_pbs
 {
     firewall_header
 
-    echo -e "\n\e[44mDeploying Backup Server Firewall Rules\e[49m"
+    echo -e "\n\e[44mDeploying Proxmox Backup Server Firewall Rules\e[49m"
 
     echo -e "\nALLOW services IN"
-        echo " - ssh       (IN)"
-            iptables -A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming ssh"
+        echo " - pbs 8007  (IN)"
+            iptables -A INPUT -p tcp -s 10.0.40.0/24,10.0.90.0/24 --dport 8007 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming pbs 8007 for devices/vpn"
 
-    # Allow Data Out -> All Handled on a per-user basis
-        echo -e "\nALLOW services OUT (per-user basis)"
-
-            # Root Only
-                echo " - ssh       root         (OUT)"
-                    iptables -A OUTPUT -p tcp --dport 22 -m owner --uid-owner root -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing ssh for root"
-                echo " - ping      root         (OUT)"
-                    iptables -A OUTPUT -p icmp --icmp-type 8 -m owner --uid-owner root -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing ping request for root"
-            
-            # Loop for services enabled across multiple accounts
-                USERS=( root _apt )
-                for U in "${USERS[@]}"
-                do
-                    echo " - http      $U         (OUT)"
-                        iptables -A OUTPUT -p tcp --dport 80 -m owner --uid-owner $U -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http for $U"
-                    echo " - https     $U         (OUT)"
-                        iptables -A OUTPUT -p tcp --dport 443 -m owner --uid-owner $U -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https for $U"
-                    echo " - dns       $U         (OUT)"
-                        iptables -A OUTPUT -p udp --dport 53 -m owner --uid-owner $U -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns for $U"
-                done
-            
+    echo -e "\nALLOW services OUT"
+        echo -e " - ACCEPT http OUT for _apt"
+            iptables -A OUTPUT -p tcp --dport 80 -m owner --uid-owner _apt -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http for _apt"
+        echo -e " - ACCEPT https OUT for _apt"
+            iptables -A OUTPUT -p tcp --dport 443 -m owner --uid-owner _apt -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https for _apt"
+        echo -e " - ACCEPT http OUT for _apt"
+            iptables -A OUTPUT -p udp --dport 53 -m owner --uid-owner _apt -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns for _apt"
+    
     firewall_persistentSave
 
     echo -e "\n\e[91mDon't Forget About Edge Firewall!\e[39m"
