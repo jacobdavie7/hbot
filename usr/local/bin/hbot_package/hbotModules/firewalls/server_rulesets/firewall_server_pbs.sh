3b60c1c8f13bc969d9615e795d1bcf4fdb25ffb5
Jacob Davie <github@accountalias.com>
2024-02-16 00:36:14 -0600

    updated pbs script

--- usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_pbs.sh
+++ usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_pbs.sh
@@ -8,16 +8,19 @@ function firewall_server_pbs
 
     echo -e "\nALLOW services IN"
         echo " - pbs 8007  (IN)"
-            iptables -A INPUT -p tcp -s 10.0.40.0/24,10.0.90.0/24 --dport 8007 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming pbs 8007 for devices/vpn"
-
+            iptables -A INPUT -p tcp -s 10.0.40.0/24,10.0.90.0/24 --dport 8007 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming pbs 8007 for pve for backups, devices/vpn"
+        echo " - icmp ping (IN)"
+            iptables -A INPUT -p icmp --icmp-type 8 -s 10.0.31.10 -m conntrack --ctstate NEW -m comment --comment "ACCEPT new incoming ping from docker server (uptime kuma)"
     echo -e "\nALLOW services OUT"
-        echo -e " - ACCEPT http OUT for _apt"
-            iptables -A OUTPUT -p tcp --dport 80 -m owner --uid-owner _apt -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http for _apt"
-        echo -e " - ACCEPT https OUT for _apt"
-            iptables -A OUTPUT -p tcp --dport 443 -m owner --uid-owner _apt -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https for _apt"
-        echo -e " - ACCEPT http OUT for _apt"
-            iptables -A OUTPUT -p udp --dport 53 -m owner --uid-owner _apt -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns for _apt"
-    
+        echo -e " - ACCEPT http OUT"
+            iptables -A OUTPUT -p tcp --dport 80 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http"
+        echo -e " - ACCEPT https OUT"
+            iptables -A OUTPUT -p tcp --dport 443 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https"
+        echo -e " - ACCEPT dns OUT"
+            iptables -A OUTPUT -p udp --dport 53 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns"
+        echo -e " - ACCEPT ntp OUT"
+            iptables -A OUTPUT -p udp --dport 123 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing NTP"
+
     firewall_persistentSave
 
     echo -e "\n\e[91mDon't Forget About Edge Firewall!\e[39m"
