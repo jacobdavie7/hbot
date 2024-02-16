80bc796ebb363640d32b447fce7b77123a0bc5e0
Jacob Davie <github@accountalias.com>
2024-02-16 00:47:53 -0600

    added pve source

--- usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_pbs.sh
+++ usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_pbs.sh
@@ -8,18 +8,18 @@ function firewall_server_pbs
 
     echo -e "\nALLOW services IN"
         echo " - pbs 8007  (IN)"
-            iptables -A INPUT -p tcp -s 10.0.40.0/24,10.0.90.0/24 --dport 8007 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming pbs 8007 for pve for backups, devices/vpn"
+            iptables -A INPUT -p tcp -s 10.0.30.10/32,10.0.40.0/24,10.0.90.0/24 --dport 8007 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming pbs 8007 for pve for backups, devices/vpn"
         echo " - icmp ping (IN)"
             iptables -A INPUT -p icmp --icmp-type 8 -s 10.0.31.10 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming ping from docker server (uptime kuma)"
     echo -e "\nALLOW services OUT"
         echo -e " - ACCEPT http OUT"
-            iptables -A OUTPUT -p tcp --dport 80 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http"
+            iptables -A OUTPUT -p tcp --dport 80 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing http"
         echo -e " - ACCEPT https OUT"
-            iptables -A OUTPUT -p tcp --dport 443 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https"
+            iptables -A OUTPUT -p tcp --dport 443 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing https"
         echo -e " - ACCEPT dns OUT"
-            iptables -A OUTPUT -p udp --dport 53 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns"
+            iptables -A OUTPUT -p udp --dport 53 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing dns"
         echo -e " - ACCEPT ntp OUT"
-            iptables -A OUTPUT -p udp --dport 123 -m owner -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing NTP"
+            iptables -A OUTPUT -p udp --dport 123 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new outgoing NTP"
 
     firewall_persistentSave
 
