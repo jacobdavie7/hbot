050bf43cf4aa978e49f174962deb1f910806cea1
Jacob Davie <github@accountalias.com>
2025-03-08 00:14:02 -0600

    added ssh

--- usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_pbs.sh
+++ usr/local/bin/hbot_package/hbotModules/firewalls/server_rulesets/firewall_server_pbs.sh
@@ -9,6 +9,8 @@ function firewall_server_pbs
     echo -e "\nALLOW services IN"
         echo " - pbs 8007  (IN)"
             iptables -A INPUT -p tcp -s 10.0.30.10/32,10.0.30.20/32,10.0.40.0/24,10.0.90.0/24 --dport 8007 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming pbs 8007 for pve for backups, devices/vpn"
+        echo " - ssh"
+            iptables -A INPUT -p tcp -s 10.0.40.0/24,10.0.90.0/24 --dport 22 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming ssh from devices/vpn"
         echo " - icmp ping (IN)"
             iptables -A INPUT -p icmp --icmp-type 8 -s 10.0.30.18 -m conntrack --ctstate NEW -j ACCEPT -m comment --comment "ACCEPT new incoming ping from docker server (uptime kuma)"
     
